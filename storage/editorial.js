248773f939d310c1ca332cbf1efa71cb335ee039
BarbatosLopux <sneideresteban@gmail.com>
2023-10-06 07:38:57 -0500

    feat: :sparkles: Agregado de las funciones para editorial
    Se agregaron las configuraciones para editorial en el cual mostrara el nombre de las mismas, la direccion y el telefono

--- storage/editorial.js
+++ storage/editorial.js
@@ -3,44 +3,44 @@ import {env} from "../config.js";
 const uri = `${env.ssl + env.hotsName}:${env.port}`;
 const config = {method: undefined, headers: {"Content-Type": "application/json"}};
 
+
+const validarExtructura = (data={})=>{
+    if(data.constructor.name !== "Object" || Object.keys(data).length==0) return {status: 400, message: `Usuario envie los datos`};
+    const {
+        nombre=null,
+        direccion=null,
+        telefono=null,
+
+    } = data;
+    if(typeof nombre !== 'string') return {status: 400, message: `El nombre '${nombre}' no cumple con el formato`};
+    if(typeof direccion !== 'string') return {status: 400, message: `La direccion '${direccion}' no cumple con el formato`};
+    if(typeof telefono !== 'number') return {status: 400, message: `El telefono '${telefono}' no cumple con el formato`};
+    return data;
+}
+export const getOne = async(id)=>{
+    config.method = "GET";
+    // config.body = "";
+    let res = await (await fetch(`${uri}/editorial/${id}`, config)).json();
+    return (Object.keys(res).length>0) ? res : {status: 401, message: "La categoria no existe", id};
+}
 export const getAll = async()=>{
     config.method = "GET";
     // config.body = "";
-    let res = await (await fetch(`${uri}/libro`, config)).json();
+    let res = await (await fetch(`${uri}/editorial`, config)).json();
     return res;
 }
-export const post = async(obj)=>{
+export const post = async(obj={})=>{
+    obj = validarExtructura(obj);
+    if(obj.status) return obj;
+    
     config.method = "POST";
     config.body = JSON.stringify(obj);
-    let res = await (await fetch(`${uri}/libro`, config)).json();
+    let res = await (await fetch(`${uri}/editorial`, config)).json();
     return res;
 }
-export const deleteOne = async(id)=>{
-    if(typeof id !== 'number') return {status: 400, message: `El datos '${id}' no cumple con el formato`};
-    config.method = "DELETE";
-    // config.body = JSON.stringify(obj);
-    let res = await (await fetch(`${uri}/libro/${id}`, config)).json();
-    return res;
-}
-export const putOne = async(obj={})=>{
-    if(!obj.id) return {status: 400, message: `Usuario porfavor ingrese los datos`};
-    const {editorialId, nombre, direccion,telefono} = obj;
-    if(typeof editorialId !== 'number') return {status: 400, message: `El datos '${autcategoriaIdorId}' no cumple con el formato`};
-    if(typeof nombre !== 'string') return {status: 400, message: `El datos '${autcategoriaIdorId}' no cumple con el formato`};
-    if(typeof direccion !== 'string') return {status: 400, message: `El datos '${direccion}' no cumple con el formato`};
-    if(typeof telefono !== 'number') return {status: 400, message: `El datos '${telefono}' no cumple con el formato`};
+console.log(await post({nombre:"Norma",
+direccion:"clle 654654#",
+telefono:3252525828}));
+// console.log(post({Direccion:"calle 62#2w-27"}));
+// console.log(post({Telefono:"3188259326"}));
 
-
-    config.method = "PUT";
-    config.body = JSON.stringify(obj);
-    let res = await (await fetch(`${uri}/libro/${id}`, config)).json();
-    return res;
-}
-
-
-
-// console.log(await post({titulo:"Pepito", fecha: "2023-08-10"}));
-// console.log(await getAll());
-// console.log(await deleteOne(1));
-
-console.log(putOne({id:"123"}));
